8c89ef6248278821486bf3f556db174acffbad48
Thiago Santos <thiagomtd2@gmail.com>
2022-08-02 14:57:40 -0300

    init project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable jsx-a11y/iframe-has-title */
 import './App.css'
 import NavBar from './ui-components/NavBar'
 import { withAuthenticator } from '@aws-amplify/ui-react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App({ user, signOut }) {
   const navbarOverrrides = {
@@ -17,14 +20,27 @@ function App({ user, signOut }) {
   }
   console.log({ user })
 
+  const [dashboardURL, setDashboardURL] = useState('')
+
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: `https://vw920578c0.execute-api.us-east-1.amazonaws.com/dev/dashboard/quicksight-url?userId=${user.username}&userName=${user.attributes.name}&userGroup=financeiro`,
+    }).then((response) => {
+      console.log(response.data.body.EmbedUrl)
+      setDashboardURL(response.data.body.EmbedUrl)
+    })
+  }, [])
+
   return (
     <div className='App'>
       <NavBar overrides={navbarOverrrides} width='100%' />
       <header className='App-header'>
-        <div id='embeddingContainer'></div>
-        <p>Nome : {user.attributes.name}</p>
-        <p>Email : {user.attributes.email}</p>
-        <p>Username : {user.username}</p>
+        <iframe
+          id='embeddingContainer'
+          width='960'
+          height='720'
+          src={dashboardURL}></iframe>
       </header>
     </div>
   )
